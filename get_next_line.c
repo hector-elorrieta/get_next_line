c9938376f90b68ceee3d6c6209f1c237fd633581
hector-elorrieta <157578150+hector-elorrieta@users.noreply.github.com>
2024-04-26 08:03:01 +0200

    cambiado bzero de posicion (por comentar) posible norma bien

--- get_next_line.c
+++ get_next_line.c
@@ -12,6 +12,15 @@
 
 #include "get_next_line.h"
 
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+		((char *)s)[i ++] = 0;
+}
+
 /**
  * Frees the memory allocated for a line.
  * 
